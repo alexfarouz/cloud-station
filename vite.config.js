fb2abae5fbef8112428942a1c322634e6436671d
Alex Farouz <alexfarouz@gmail.com>
2024-05-23 18:43:42 -0400

    Modify vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cloud-station/',
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000
-  }
 })
